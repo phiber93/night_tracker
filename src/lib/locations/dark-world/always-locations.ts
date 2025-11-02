d0576bc2507059f679bb929321df85295208fa6e
phiber93 <153130604+phiber93@users.noreply.github.com>
2025-11-02 21:00:01 +0100

    add dw always locations

--- src/lib/locations/dark-world/always-locations.ts
+++ src/lib/locations/dark-world/always-locations.ts
@@ -6,4 +6,24 @@ export const alwaysLocationsDarkworld: IItemLocation[] = [
     top: 68,
     left: 4,
   },
+  {
+    name: 'Stumpy',
+    top: 66,
+    left: 29.7,
+  },
+  {
+    name: 'Bumper Ledge',
+    top: 14,
+    left: 32.5,
+  },
+  {
+    name: 'Catfish',
+    top: 15,
+    left: 90,
+  },
+  {
+    name: 'Pyramid Ledge',
+    top: 44,
+    left: 57,
+  },
 ];
